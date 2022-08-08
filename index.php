4f745e4554ee610fd8e83485e7fd241635912646
Leonardo Perin de Berso <leonardoperin70@gmail.com>
2022-08-08 15:51:05 -0300

    Add o index.php e o retorn.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<html lang= "pt-br">
+ <head>
+  <title>TRABALHO</title>
+ </head>
+ <body style="background:tomato;">
+ <h3>Cadastro Pessoa</h3>
+    <form action="retorn.php" method="post">
+        <label for="nomePe"></label><br>
+        <input type="text" id="nomePe" name="nomePe" value=""><br>
+
+    <h3>Cadastro Setor</h3>
+    
+        <label for="nomeSet"></label><br>
+        <input type="text" id="nome" name="nomeSet" value=""><br>
+          
+    <h3>Cadastro Função</h3>
+   
+        <label for="nomeFun"></label><br>
+        <input type="text" id="nome" name="nomeFun" value=""><br>
+        
+    <h3>Cadastro Contato</h3>
+    
+        <label for="nomeCon"></label><br>
+        <input type="text" id="endereco" name="endeCon" value=""><br>
+         
+    <h3>Cadastro Tipo de Contato</h3>
+    
+        <label for="nomeTiCo"></label><br>
+        <input type="text" id="nome" name="tipoCon" value=""><br>
+        
+        <input type="submit" value="Enviar">   
+        
+    </form>
+
+ </body>
+</html>
---
+++ retorn.php
@@ -0,0 +1,5 @@
+<p>Nome da Pessoa: <?php echo htmlspecialchars($_POST['nomePe']); ?></p>
+<p>Nome do Setor: <?php echo htmlspecialchars($_POST['nomeSet']); ?></p>
+<p>Nome da Função: <?php echo htmlspecialchars($_POST['nomeFun']); ?></p>
+<p>Endereço de Contato: <?php echo htmlspecialchars($_POST['endeCon']); ?></p>
+<p>Tipo de Contato: <?php echo htmlspecialchars($_POST['tipoCon']); ?></p>
